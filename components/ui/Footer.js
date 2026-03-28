74915630f8e1dedac1ae8ee1b8e2db21fb3135b6
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-29 00:24:54 +0800

    initial set up

--- components/ui/Footer.js
+++ components/ui/Footer.js
@@ -3,7 +3,8 @@ import Link from 'next/link';
 const Footer = () => {
     explore:[
         { name: 'Home', href: '/' },
-        
+        { name: 'Properties', href: '/properties' },
+
     ], 
     operations: [
 
