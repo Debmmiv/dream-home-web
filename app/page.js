d1505dd28f645cd4bad355c9716d7bf9d4d11084
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-30 17:00:59 +0800

    cleaning

--- app/page.js
+++ app/page.js
@@ -1,12 +1,6 @@
 export default function Home() {
   return (
     <div className="p-10 bg-gray-50 min-h-screen">
-      <h1 className="text-4xl font-extrabold text-gray-950">
-        Welcome to DreamHome
-      </h1>
-      <p className="mt-4 text-lg text-gray-700 max-w-2xl">
-        Discover unparalleled hotels, unique rentals, and unforgettable experiences. Your dream stay starts here.
-      </p>
     </div>
   );
 }
